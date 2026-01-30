6ee3dd23d4f7e230eadd2b1b58f1262fd748d8c9
AmithReddy7 <reddyamith259@gmail.com>
2026-01-30 16:02:56 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,8 @@
+import sys
+n1=int(sys.argv[1])
+n1=int(sys.argv[2])
+print("Addition",n1+n2)
+print("Subtraction",n1-n2)
+print("Multiplication",n1*n2)
+print("Division",n1/n2)
+print("Changes are same in python file")
