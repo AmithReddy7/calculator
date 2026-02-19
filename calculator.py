1cc2870a89a545775311f1d6efc3ecaf5e40f4bf
AmithReddy7 <reddyamith259@gmail.com>
2026-02-19 09:50:24 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,8 +1,21 @@
 import sys
-n1=int(sys.argv[1])
-n1=int(sys.argv[2])
-print("Addition",n1+n2)
-print("Subtraction",n1-n2)
-print("Multiplication",n1*n2)
-print("Division",n1/n2)
-print("Changes are same in python file")
+
+if len(sys.argv) != 3:
+    print("Please provide exactly two numbers.")
+    sys.exit()
+
+try:
+    n1 = int(sys.argv[1])
+    n2 = int(sys.argv[2])
+
+    print("Addition:", n1 + n2)
+    print("Subtraction:", n1 - n2)
+    print("Multiplication:", n1 * n2)
+
+    if n2 != 0:
+        print("Division:", n1 / n2)
+    else:
+        print("Division: Cannot divide by zero.")
+
+except ValueError:
+    print("Please enter valid integers.")
