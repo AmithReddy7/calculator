04e7af0cfe7aef5f01fa46ef2927609cf6edee37
AmithReddy7 <reddyamith259@gmail.com>
2026-02-19 09:48:03 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,27 @@
-class Calculator
-  {
-    public static void main(String args[])
-    {
-      int n1=Integer.parseInt(args[0]);
-      int n2=Integer.parseInt(args[1]);
-      int r1=n1+n2;
-      int r2=n1-n2;
-      int r3=n1*n2;
-      int r4=n1/n2;
-      System.out.println("Addition:"+r1); 
-      System.out.println("Subtraction:"+r2); 
-      System.out.println("Multiplication:"+r3); 
-      System.out.println("Division:"+r4);
+class Calculator {
+  public static void main(String args[]) {
+
+    if (args.length != 2) {
+      System.out.println("Please provide exactly two integer values.");
+      return;
+    }
+
+    try {
+      int n1 = Integer.parseInt(args[0]);
+      int n2 = Integer.parseInt(args[1]);
+
+      System.out.println("Addition: " + (n1 + n2));
+      System.out.println("Subtraction: " + (n1 - n2));
+      System.out.println("Multiplication: " + (n1 * n2));
+
+      if (n2 != 0) {
+        System.out.println("Division: " + (n1 / n2));
+      } else {
+        System.out.println("Division: Cannot divide by zero.");
+      }
+
+    } catch (NumberFormatException e) {
+      System.out.println("Please enter valid integers.");
     }
   }
-      
-      
-      
-      
-      
+}
